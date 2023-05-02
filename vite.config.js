e79d4ccf169df687015542fef31e28f5202633aa
BrandonCap <brandoncappo@gmail.com>
2023-05-02 02:18:32 -0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/BrandonCap.github.io",
+  base: "/BrandonCap.github.io/",
   plugins: [svelte()],
 })
